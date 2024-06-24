f62f9d1cf4122e94792dbae19d2c1ffcb2a707b1
Hyun-Ji <guswl543210@gmail.com>
2024-06-25 04:37:04 +0900

    2024-06-25_업데이트

--- src/views/page/Auth.jsx
+++ src/views/page/Auth.jsx
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from "react";
 import logo from "../../image/trash.png";
 export const Auth = () => {
-        const [userId, setUserId] = useState('');
-        const [password, setPassword] = useState('');
-      
-        const onSubmit = async (e) => {
-          e.preventDefault();
-          try {
-           console.log("하이")
-          } catch (error) {
-            console.log(error);
-          }
-        };
+  const [userId, setUserId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log("하이");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="main login">
       <div className="login_box">
-      <img src={logo} alt="logo" />
-      <h2>감쓰통</h2>
-      <h3>당신을 위한 감정 쓰레기통</h3>
+        <div className="title_wrap">
+          <img src={logo} alt="logo" />
+          <h2>감쓰통</h2>
+          <h3>당신을 위한 감정 쓰레기통</h3>
+        </div>
         <form onSubmit={onSubmit}>
           <input
             type="text"
@@ -39,8 +41,12 @@ export const Auth = () => {
           <button type="submit" className="btn">
             로그인
           </button>
-          <p>아직 계정이 없으신가요? <span>회원가입하기</span></p>
-          <p><span>아이디 | 비밀번호</span> 찾기</p>
+          <p>
+            아직 계정이 없으신가요? <span>회원가입하기</span>
+          </p>
+          <p>
+            <span>아이디 | 비밀번호</span> 찾기
+          </p>
         </form>
       </div>
     </div>
